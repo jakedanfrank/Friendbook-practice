0525a27aa05b3e085db220cc9386651f320db656
Jake Frank <jacob423frank@gmail.com>
2020-03-03 22:52:33 -0700

    first commit, people controller

---
+++ db/migrate/20200304044611_create_people.rb
@@ -0,0 +1,14 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :avatar
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
